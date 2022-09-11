7a7d962d22db884077e363c774a6306f0c709ec2
lennyomg <lennyomg@users.noreply.github.com>
2022-09-10 20:21:09 -0700

    added Get-SpotifyRecommendations

--- Spotify.psm1
+++ Spotify.psm1
@@ -1283,6 +1283,269 @@ function Add-SpotifyTrack {
 
 <#
 .SYNOPSIS
+Get recommendations.
+
+.DESCRIPTION
+Recommendations are generated based on the available information for a given seed entity and matched against similar artists and tracks. 
+If there is sufficient information about the provided seeds, a list of tracks will be returned together with pool size details.
+For artists and tracks that are very new or obscure there might not be enough data to generate a list of tracks.
+
+.EXAMPLE
+Get-SpotifyRecommendations -SeedArtists "2CIMQHirSU0MQqyYHq0eOx", "57dN52uHvrHOxijzpIgu3E" -MinEnergy 0.8 -MaxEnergy 1 -TargetDanceability 1
+
+.LINK
+https://developer.spotify.com/documentation/web-api/reference/#/operations/get-recommendations
+#>
+function Get-SpotifyRecommendations {
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+
+        # A list of Spotify IDs for seed artists. Up to 5 seed values may be provided in any combination of seed_artists, seed_tracks and seed_genres. Example value: "4NHQUGzhtTLFvgF5SZesLK".
+        [Parameter()]
+        [ValidateCount(0, 5)]
+        [Alias("seed_artists")]
+        [string[]] $SeedArtists,
+        
+        # A list of any genres in the set of available genre seeds. Up to 5 seed values may be provided in any combination of seed_artists, seed_tracks and seed_genres. Example value: "classical,country".
+        [Parameter()]
+        [ValidateCount(0, 5)]
+        [Alias("seed_genres")]
+        [string[]] $SeedGenres,
+
+        # A list of Spotify IDs for a seed track. Up to 5 seed values may be provided in any combination of seed_artists, seed_tracks and seed_genres. Example value: "0c6xIDDpzE81m2q797ordA".
+        [Parameter()]
+        [ValidateCount(0, 5)]
+        [Alias("seed_tracks")]
+        [string[]] $SeedTracks,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("max_acousticness")]
+        [double] $MaxAcousticness,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("max_danceability")]
+        [double] $MaxDanceability,
+
+        # Integer
+        [Parameter()]
+        [Alias("max_duration_ms")]
+        [int] $MaxDurationMs,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("max_energy")]
+        [double] $MaxEnergy,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("max_instrumentalness")]
+        [double] $MaxInstrumentalness,
+
+        # Integer 0..11
+        [Parameter()]
+        [Alias("max_key")]
+        [int] $MaxKey,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("max_liveness")]
+        [double] $MaxLiveness,
+
+        # Double
+        [Parameter()]
+        [Alias("max_loudness")]
+        [double] $MaxLoudness,
+
+        # Integer 0..1
+        [Parameter()]
+        [Alias("max_mode")]
+        [int] $MaxMode,
+
+        # Integer 0..100
+        [Parameter()]
+        [Alias("max_popularity")]
+        [int] $MaxPopularity,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("max_speechiness")]
+        [double] $MaxSpeechiness,
+
+        # Double (bpm)
+        [Parameter()]
+        [Alias("max_tempo")]
+        [double] $MaxTempo,
+
+        # Integer
+        [Parameter()]
+        [Alias("max_time_signature")]
+        [int] $MaxTimeSignature,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("max_valence")]
+        [double] $MaxValence,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_acousticness")]
+        [double] $MinAcousticness,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_danceability")]
+        [double] $MinDanceability,
+
+        # Integer
+        [Parameter()]
+        [Alias("min_duration_ms")]
+        [int] $MinDurationMs,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_energy")]
+        [double] $MinEnergy,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_instrumentalness")]
+        [double] $MinInstrumentalness,
+
+        # Integer 0..11
+        [Parameter()]
+        [Alias("min_key")]
+        [int] $MinKey,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_liveness")]
+        [double] $MinLiveness,
+
+        # Double
+        [Parameter()]
+        [Alias("min_loudness")]
+        [double] $MinLoudness,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_mode")]
+        [int] $MinMode,
+
+        # Integer 0..100
+        [Parameter()]
+        [Alias("min_popularity")]
+        [int] $MinPopularity,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_speechiness")]
+        [double] $MinSpeechiness,
+
+        # Double
+        [Parameter()]
+        [Alias("min_tempo")]
+        [double] $MinTempo,
+
+        # Integer
+        [Parameter()]
+        [Alias("min_time_signature")]
+        [int] $MinTimeSignature,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("min_valence")]
+        [double] $MinValence,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("target_acousticness")]
+        [double] $TargetAcousticness,
+ 
+        # Double 0..1
+        [Parameter()]
+        [Alias("target_danceability")]
+        [double] $TargetDanceability,
+
+        # Integer
+        [Parameter()]
+        [Alias("target_duration_ms")]
+        [int] $TargetDurationMs,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("target_energy")]
+        [double] $TargetEnergy,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("target_instrumentalness")]
+        [double] $TargetInstrumentalness,
+
+        # Integer 0..11
+        [Parameter()]
+        [Alias("target_key")]
+        [int] $TargetKey,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("target_liveness")]
+        [double] $TargetLiveness,
+
+        # Double
+        [Parameter()]
+        [Alias("target_loudness")]
+        [double] $TargetLoudness,
+
+        # Integer 0..1
+        [Parameter()]
+        [Alias("target_mode")]
+        [int] $TargetMode,
+
+        # Integer 0..100
+        [Parameter()]
+        [Alias("target_popularity")]
+        [int] $TargetPopularity,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("target_speechiness")]
+        [double] $TargetSpeechiness,
+
+        # Double (bpm)
+        [Parameter()]
+        [Alias("target_tempo")]
+        [double] $TargetTempo,
+
+        # Integer
+        [Parameter()]
+        [Alias("target_time_signature")]
+        [int] $TargetTimeSignature,
+
+        # Double 0..1
+        [Parameter()]
+        [Alias("target_valence")]
+        [double] $TargetValence
+    )
+
+    $e = @{}
+    $c = Get-Command Get-SpotifyRecommendations
+    foreach ($p in $PSBoundParameters.Keys) {
+        $v = $PSBoundParameters[$p]
+        $e[$c.Parameters[$p].Aliases[0]] = $v.Count ? $v -join "," : $v
+    }
+    
+    Invoke-RestMethod `
+        -Uri "https://api.spotify.com/v1/recommendations?limit=100&$(($e.Keys | ForEach-Object { "$($_)=$($e[$_])" }) -join "&")" `
+        -Method Get `
+        -Authentication Bearer `
+        -Token $global:SpotifyToken `
+        -ContentType "application/json"
+}
+
+<#
+.SYNOPSIS
 Get user's saved tracks.
 
 .DESCRIPTION
---
+++ functions/tracks/Get-SpotifyRecommendations.Tests.ps1
@@ -0,0 +1,78 @@
+BeforeAll { 
+    . $PSScriptRoot/Get-SpotifyRecommendations.ps1
+}
+
+Describe "Get-SpotifyRecommendations" {
+    It "Default" {
+        $p = Get-SpotifyRecommendations -SeedArtists "2CIMQHirSU0MQqyYHq0eOx", "57dN52uHvrHOxijzpIgu3E" -MinEnergy 0.8 -MaxEnergy 1 -TargetDanceability 1
+        $p.seeds | Should -Not -BeNullOrEmpty
+        $p.tracks.Count | Should -BeGreaterThan 0
+    }
+}
+
+Describe "Get-SpotifyRecommendations Syntax" {
+    It "All" {
+        Mock Invoke-RestMethod
+        Get-SpotifyRecommendations `
+            -SeedArtists "a1", "a2" `
+            -SeedGenres "g1", "g2" `
+            -SeedTracks "t1", "t2" `
+            -MaxAcousticness 0 `
+            -MaxDanceability 0 `
+            -MaxDurationMs 0 `
+            -MaxEnergy 0 `
+            -MaxInstrumentalness 0 `
+            -MaxKey 0 `
+            -MaxLiveness 0 `
+            -MaxLoudness 0 `
+            -MaxMode 0 `
+            -MaxPopularity 0 `
+            -MaxSpeechiness 0 `
+            -MaxTempo 0 `
+            -MaxTimeSignature 0 `
+            -MaxValence 0 `
+            -MinAcousticness 0 `
+            -MinDanceability 0 `
+            -MinDurationMs 0 `
+            -MinEnergy 0 `
+            -MinInstrumentalness 0 `
+            -MinKey 0 `
+            -MinLiveness 0 `
+            -MinLoudness 0 `
+            -MinMode 0 `
+            -MinPopularity 0 `
+            -MinSpeechiness 0 `
+            -MinTempo 0 `
+            -MinTimeSignature 0 `
+            -MinValence 0 `
+            -TargetAcousticness 0 `
+            -TargetDanceability 0 `
+            -TargetDurationMs 0 `
+            -TargetEnergy 0 `
+            -TargetInstrumentalness 0 `
+            -TargetKey 0 `
+            -TargetLiveness 0 `
+            -TargetLoudness 0 `
+            -TargetMode 0 `
+            -TargetPopularity 0 `
+            -TargetSpeechiness 0 `
+            -TargetTempo 0 `
+            -TargetTimeSignature 0 `
+            -TargetValence 0 
+    }
+
+    Test-Validation { Get-SpotifyRecommendations -SeedArtists "1", "2", "3", "4", "5", "6" }
+    Test-Validation { Get-SpotifyRecommendations -SeedGenres "1", "2", "3", "4", "5", "6" }
+    Test-Validation { Get-SpotifyRecommendations -SeedTracks "1", "2", "3", "4", "5", "6" }
+
+    It "Alias" {
+        $c = Get-Command Get-SpotifyRecommendations
+        $c.Parameters.Values | ForEach-Object {
+            $_.Aliases.Count | Should -BeGreaterThan 0 -Because $_.Name 
+        }
+    }
+}
+
+Describe "Get-SpotifyRecommendations Output" -Tag "Output" {
+    Update-CommandOutput { Get-SpotifyRecommendations -SeedArtists "2CIMQHirSU0MQqyYHq0eOx", "57dN52uHvrHOxijzpIgu3E" -MinEnergy 0.8 -MaxEnergy 1 -TargetDanceability 1 }
+}
